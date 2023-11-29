73d059561778eb7dcfb859762c65e6cc1760557b
Noman Rauf <nomi589@hotmail.com>
2023-11-29 15:46:08 +0500

    Configure firebase

---
+++ src/helpers/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqVJw0XN9lP2gi8UNek66KI2Zyccmm-8M",
+  authDomain: "potato-chat-32218.firebaseapp.com",
+  projectId: "potato-chat-32218",
+  storageBucket: "potato-chat-32218.appspot.com",
+  messagingSenderId: "850911765249",
+  appId: "1:850911765249:web:64d10fef983a96ccc1b100",
+};
+
+const app = initializeApp(firebaseConfig);
